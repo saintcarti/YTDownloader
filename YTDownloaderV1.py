f7df90675ed3e694778ff08b24ef91f01448846d
saintcarti <134324891+saintcarti@users.noreply.github.com>
2024-03-25 21:19:23 -0300

    Actualizacion a App YTDownloaderV1.py
    Finalizacion de proyecto personal me doy por cumplido esto ya que aprendi nuevas funciones y formas de manejar excepciones errores condicionales con py

--- YTDownloaderV1.py
+++ YTDownloaderV1.py
@@ -1,11 +1,13 @@
 from tkinter import *
 from pytube import YouTube
 from tkinter import messagebox
+from moviepy.editor import VideoFileClip
 import os
-from pydub import AudioSegment
+import re
 
+numero_audio = 0
+numero_video = 0
 
-AudioSegment.ffmpeg = "C:\\Users\\Xavier\\Desktop\\ffmg\\ffmpeg-6.1.1"
 root = Tk()
 root.geometry('500x300')
 root.resizable(0,0)
@@ -22,12 +24,22 @@ link_enter=Entry(root,width=70,
                  textvariable=link).place(x=32, y =120)
 
 def downloader_mp4():
-    download_video(merge_audio=False)
+    global numero_video
+    download_video(merge_audio=False ,numero = numero_video)
+    numero_video += 1
 
 def downloader_mp3():
-    download_video(merge_audio=True)
-    
-def download_video(merge_audio=False):
+    global numero_audio
+    download_video(merge_audio=True ,numero = numero_audio)
+    numero_audio += 1
+
+def clean_file(filename):
+    cleaned_filename = re.sub(r'[\\/*?:<>|]','',filename)
+    cleaned_filename = re.sub(r'\s+','_',cleaned_filename)
+    return cleaned_filename
+
+
+def download_video(merge_audio=False , numero= None):
     try:
         video_link = str(link.get()).strip()
 
@@ -36,7 +48,7 @@ def download_video(merge_audio=False):
         
         url = YouTube(video_link)
         url.check_availability()
-        video = url.streams.get_by_resolution(resolution=1080)
+        video = url.streams.get_highest_resolution()
 
         user_home_dir = os.path.expanduser("~")
 
@@ -49,16 +61,24 @@ def download_video(merge_audio=False):
 
         
         if merge_audio:
-            audio_file_path = os.path.join(folder_path, f"{url.title}.mp3")
-            video.download(output_path=folder_path,filename="temp_video.mp4")
-            video_path = os.path.join(folder_path, "temp_video.mp4")
-            audio_path = os.path.join(folder_path, f"{url.title}.mp3")
+            audio_file_path = os.path.join(
+                folder_path, f"audio{numero}.mp3")
+            video.download(
+                output_path=folder_path,filename="temp_video.mp4")
+            video_path = os.path.join(
+                folder_path, "temp_video.mp4")
+            audio_path = os.path.join(
+                folder_path, f"audio{numero}.mp3")
             convert_to_mp3(video_path, audio_path)
             os.remove(video_path)
+            messagebox.showinfo('Hecho!!','Audio descargado con exito')
         else:
-            file_name = os.path.join(folder_path, f"{url.title}.mp4")
-            video.download(output_path=folder_path, filename=url.title+".mp4")
-        messagebox.showinfo('Hecho ','El video ha sido descargado con exito')
+            file_name = os.path.join(
+                folder_path, f"video{numero}.mp4")
+            video.download(output_path=folder_path,
+                            filename=f"video{numero}"+".mp4")
+            messagebox.showinfo('Hecho ',
+                            'El video ha sido descargado con exito')
 
         Label(root , text='Descargado', font='arial 13 bold',
             bg='#AACDE2',fg='#B57199').place(x=350,y=210)
@@ -66,8 +86,11 @@ def download_video(merge_audio=False):
         messagebox.showerror('Error', str(e))
 
 def convert_to_mp3(video_path,audio_path):
-    video = AudioSegment.from_file(video_path,"mp4")
-    video.export(audio_path, format="mp3")
+    video_clip = VideoFileClip(video_path)
+    audio_clip = video_clip.audio
+    audio_clip.write_audiofile(audio_path)
+    audio_clip.close()
+    video_clip.close()
 
 
 def ExitProgram():
